9e995422c15e5fb47ced182a35b5d2088e41a10c
Chayan Shrang Raj <chayansraj95@gmail.com>
2023-08-10 22:14:28 +0200

    Create de_youtube_etl_csv_to_parquet.py
    This file converts the CSV file to parquet files to create data consistency in parquet

---
+++ de_youtube_etl_csv_to_parquet.py
@@ -0,0 +1,72 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+
+
+# Script generated for node raw_data
+raw_data_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={
+        "quoteChar": '"',
+        "withHeader": True,
+        "separator": ",",
+        "optimizePerformance": False,
+    },
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://data-engineering-youtube-raw-us-east-1-dev/raw_statistics/"],
+        "recurse": True,
+    },
+    transformation_ctx="raw_data_node1",
+)
+
+# Script generated for node ApplyMapping
+ApplyMapping_node2 = ApplyMapping.apply(
+    frame=raw_data_node1,
+    mappings=[
+        ("video_id", "string", "video_id", "string"),
+        ("trending_date", "string", "trending_date", "string"),
+        ("title", "string", "title", "string"),
+        ("channel_title", "string", "channel_title", "string"),
+        ("category_id", "string", "category_id", "bigint"),
+        ("publish_time", "string", "publish_time", "string"),
+        ("tags", "string", "tags", "string"),
+        ("views", "string", "views", "bigint"),
+        ("likes", "string", "likes", "bigint"),
+        ("dislikes", "string", "dislikes", "bigint"),
+        ("comment_count", "string", "comment_count", "bigint"),
+        ("thumbnail_link", "string", "thumbnail_link", "string"),
+        ("comments_disabled", "string", "comments_disabled", "boolean"),
+        ("ratings_disabled", "string", "ratings_disabled", "boolean"),
+        ("video_error_or_removed", "string", "video_error_or_removed", "boolean"),
+        ("description", "string", "description", "string"),
+        ("region", "string", "region", "string"),
+    ],
+    transformation_ctx="ApplyMapping_node2",
+)
+
+# Script generated for node transformed_data
+transformed_data_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=ApplyMapping_node2,
+    connection_type="s3",
+    format="glueparquet",
+    connection_options={
+        "path": "s3://data-engineering-youtube-cleaned-us-east-1-dev/youtube/raw_statistics/",
+        "partitionKeys": ["region"],
+    },
+    format_options={"compression": "snappy"},
+    transformation_ctx="transformed_data_node3",
+)
+
+job.commit()
